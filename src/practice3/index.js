f1d5904c376e98b234f0b75c6d34d3fedcd6c46b
XiongqiChang <2838159948@qq.com>
2020-10-15 21:57:09 +0800

    完成js部分

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,24 @@
-export const getPoetry
+export const getPoetry = function () {
+    
+    const promise = new Promise((resolve, reject) => {
+        fetch(
+            "https://v1.jinrishici.com/all.json", {
+            method: "GET",
+            headers: {
+                accept: "text/html",
+            },
+        }
+        ).then((result) => {
+            return result.json();
+        })
+        .then((pom) => {
+                const poem = [];
+                poem.push(pom.origin);
+                poem.push(pom.author);
+                poem.push(pom.content);
+                resolve(poem)
+            })
+        .catch((err) => reject(err));
+    });
+    return promise;
+}
